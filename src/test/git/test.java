e41f29b5abcca226b569079515fc2446d6b893bf
jatin <jatin@gmail.com>
2017-06-29 15:39:52 +0530

    add method

--- src/test/git/test.java
+++ src/test/git/test.java
@@ -3,6 +3,12 @@ package test.git;
 public class test {
 
 	public static void main(String[] args) {
-		System.out.println("hello.......");
+		System.out.println("hello......."+Hello());
+	}
+	
+	public static String Hello(){
+		String name="jatin";
+		
+		return name;
 	}
 }
